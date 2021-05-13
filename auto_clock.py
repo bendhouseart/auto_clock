422a0ea16539ed9a3d5c6fb93196753cead0b380
Anthony Galassi <2317139-anthonygalassi@users.noreply.gitlab.com>
2021-05-13 09:54:21 -0700

    added cc

--- auto_clock.py
+++ auto_clock.py
@@ -19,6 +19,7 @@ dotenv.load_dotenv(dotenv.find_dotenv())
 port = os.environ.get("port_number")
 password = os.environ.get("email_password")
 receiver_email = os.environ.get("receiver_email")
+cc_email = os.environ.get("cc_email")
 sender_email = os.environ.get("sender_email")
 mail_server = os.environ.get("mail_server")
 
@@ -27,10 +28,13 @@ context = ssl.create_default_context()
 email_message = message.Message()
 email_message.add_header('from', sender_email)
 email_message.add_header('to', receiver_email)
+email_message.add_header('cc', cc_email)
 email_message.add_header('subject', subject)
 email_message.set_payload(f'{subject}\n')
+
+to_addrs = [receiver_email, cc_email]
 
 
 with smtplib.SMTP_SSL(mail_server, port, context=context) as server:
     server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, email_message.as_string())
+    server.sendmail(sender_email, to_addrs, email_message.as_string())
